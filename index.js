4cdff9fa7ac10697943209851095e9d66965d6df
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-06-15 00:47:48 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email){
+    const configurationObject = {
+        method : 'POST',
+        headers : {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body : JSON.stringify({
+            'name' : name,
+            'email' : email,
+        })
+    }
+    //console.log(configurationObject)
+    const fetchedData = fetch(`http://localhost:3000/users`, configurationObject)
+        .then(response => response.json())
+        .then(function(user){
+            document.body.innerHTML = `<p>${user.id}</p>`
+        })
+        .catch(function(error){
+            document.body.innerHTML = `<p>${error.message}</p>`
+        })
+    return fetchedData;
+}
+
+submitData('Chief', 'chief@cg')
